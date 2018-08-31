d19a8474e729904987c88e00207e563c0d22d8c0
Voilance <voilance@qq.com>
2018-08-31 13:03:49 +0800

    Initial.

---
+++ app/src/main/java/com/example/bz/bz/act_main.java
@@ -0,0 +1,13 @@
+package com.example.bz.bz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class act_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_main);
+    }
+}
