55e6f0eaab53ca1b37be654afd54f186e23bb1ba
Otto Tseng <1664647331@qq.com>
2018-09-01 19:38:56 +0800

    主要修改mooc fragment

--- app/src/main/java/com/example/bz/bz/MoocFragment.java
+++ app/src/main/java/com/example/bz/bz/MoocFragment.java
@@ -8,11 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MoocFragment extends Fragment {
+public class MoocFragment extends Fragment implements View.OnClickListener{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.mooc_fragment_test,container,false);
+        View view=inflater.inflate(R.layout.mooc_fragment,container,false);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_type_1:
+                break;
+            default:
+                break;
+        }
     }
 }
